fee5eae68ea5f48446bf2bb562520d80816aa731
Glindley23 <graham.lindley23@gmail.com>
2022-11-30 21:37:03 -0500

    added home view

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+//Defines the default engine we will be using (JSX)
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // /places sets all routes in the places controller relative to /places (any other path will have /places in front for this controller)
 app.use('/places', require('./controllers/places'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('home')
 });
 
 app.get('*', (req, res) => {
@@ -14,3 +18,13 @@ app.get('*', (req, res) => {
 });
 
 app.listen(process.env.PORT);
+
+
+
+
+
+
+
+
+
+//http//:localhost:3000/
