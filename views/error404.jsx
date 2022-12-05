c8c26a124cdeced417c287bbe45a197726b48f6d
Glindley23 <graham.lindley23@gmail.com>
2022-12-04 19:54:09 -0500

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,7 +6,14 @@ function error404 () {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
-              <p>Oops, sorry, we can't find this page!</p>
+              <p>
+                Oops, sorry, we can't find this page!
+              </p>
+              <img id='error-img' src='/images/icecream.jpg' alt='ice cream cone fell over' />
+              <div>
+                Photo by <a href="https://unsplash.com/@rojekilian?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sarah Kilian</a> on <a href="https://unsplash.com/s/photos/error?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+              </div>
+  
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,12 +6,20 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src='/images/vegetables.jpg' alt='vegetables in baskets' />
+                </div>
+                <div>
+                Photo by <a href="https://unsplash.com/@organicdesignco?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Megan Thomas</a> on <a href="https://unsplash.com/s/photos/agriculture?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
                 <a href='/places'>
                     <button className='btn-primary'>Places Page</button>
                 </a>
+
             </main>
         </Def>
     )
 } 
 
 module.exports = home 
+
