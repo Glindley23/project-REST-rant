8ebe3342d60ab09c1da55ce0c030a13e56e20ae1
Glindley23 <graham.lindley23@gmail.com>
2022-12-07 22:26:59 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -6,9 +6,9 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 });
 
+//POST: Default info if none is provided
 router.post('/', (req, res) => {
   if (!req.body.pic) {
-    // Default image if one is not provided
     req.body.pic = 'http://placekitten.com/400/400'
   }
   if (!req.body.city) {
@@ -21,11 +21,21 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
-
+//GET: New place page
 router.get('/new', (req, res) => {
   res.render('places/new')
 });
 
-
+//GET: Show route 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  } else if (!places[id]) {
+    res.render('error404')
+  } else {
+    res.render('places/show', {place: places[id]})
+  }
+})
 
 module.exports = router;
