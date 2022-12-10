329cb0768785d789655158e16fa17e1c6cc8867f
Glindley23 <graham.lindley23@gmail.com>
2022-12-09 23:20:11 -0500

    Added edit form and PUT route

--- controllers/places.js
+++ controllers/places.js
@@ -50,4 +50,30 @@ router.delete('/:id', (req, res) => {
   }
 })
 
+router.get('/:id/edit'), (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  } else if (!places[id]) {
+    res.render('error404')
+  } else {
+    res.render('places/edit', {place: places[id]}, id)
+  
+  }
+}
+
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  } else if (!places[id]) {
+    res.render('error404')
+  } else {
+    res.redirect(`/places/${id}`)
+  }
+})
+
+
+
+
 module.exports = router;
